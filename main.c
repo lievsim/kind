82a351e77ab6d40ab3af72eee5b4c8daa0572f50
Simon Lievre <simon.lievre@gmail.com>
2019-04-10 19:16:14 +0200

    Added target library in CMakeLists.txt. Added opening database in main.c

--- main.c
+++ main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <signal.h>
 #include <stdbool.h>
+#include <sodium.h>
+#include <string.h>
 #include "state.h"
 #include "cmd.h"
 
+#define KEYSIZE 256
+
 void sigHandler(int signum){
     printf("Caught %d, exiting...", signum);
-    exit(1);
+    exit(EXIT_SUCCESS);
 }
 
 void help(){
@@ -20,31 +24,56 @@ void help(){
 
 int main() {
 
-    // TODO: libsodium init
+    void *masterKey;
+    FILE *db;
+
+    // Initializing libsodium library
+    if (sodium_init() < 0) {
+        printf("Fatal Error. Exiting...");
+        exit(EXIT_FAILURE);
+    }
 
     // Registering a signal handler
     signal(SIGINT, sigHandler);
 
-    // TODO: libsodium malloc
-    char masterPwd[128];
+    // Opening database
+    char *filename = getenv("HOME");
+    strcat(filename, "/.kind/db.txt");
+    db = fopen(filename, "rw");
+    if(db == NULL){
+        printf("Database not found under %s\n", filename);
+        char answer;
+        printf("Do you want to create a new one [y|n]? ");
+        do{
+            scanf(" %c", &answer);
+        }while(answer != 'y' && answer != 'n');
+        if(answer == 'y'){
+            // TODO: create db
+        }else{
+            printf("Ok this is your choice. Exiting now...");
+            exit(EXIT_SUCCESS);
+        }
+    }
+    masterKey = sodium_malloc(KEYSIZE);
 
     // Final State Machine (FSM)
     enum state s = LOCKED;
     while(true){
 
         if(s == LOCKED){
-
+            char *masterPwd = sodium_malloc(KEYSIZE);
             printf("Master password: ");
             scanf("%s", masterPwd);
             // TODO: PPKDF argon2 symetric key
             s = UNLOCKED;
 
         }else if(s == UNLOCKED) {
+            printf("\e[1;1H\e[2J");
             help();
-            int cmd = -1;
+            char cmd;
+            printf("Enter a command [%d-%d]: ", EXI, LST);
             do {
-                printf("Enter a command [%d-%d]: ", EXI, LST);
-                scanf("%d", &cmd);
+                scanf(" %c", &cmd);
             } while (cmd < EXI || cmd > LST);
 
             if (cmd == EXI) {
